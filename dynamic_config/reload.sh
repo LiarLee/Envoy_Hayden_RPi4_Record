c59097a22a611d80f1ad8109b68dc340fce41789
LiarLee <625610036@qq.com>
2022-06-26 00:42:52 +0800

    20220626-add jeager and tracing add-on

--- dynamic_config/reload.sh
+++ dynamic_config/reload.sh
@@ -3,6 +3,12 @@
 
 DIR=/opt/ingressgateway-envoy/dynamic_config
 
+echo "! Backup Config ... "
+cp -pv $DIR/cds.yaml $DIR/backup_config/cds.yaml-"`date`".bak
+cp -pv $DIR/lds.yaml $DIR/backup_config/lds.yaml-"`date`".bak
+
+echo ""
+echo "! Let it Go ... "
 mv -v $DIR/cds.yaml $DIR/123
 mv -v $DIR/123 $DIR/cds.yaml
 
